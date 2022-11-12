e12d505be641f14b462ede0dbef9ecdc353eaf24
wayusei <r.roszam@gmail.com>
2022-11-11 22:15:57 -0600

    agregada la sesion 8 desde el repo de andrés

---
+++ S8-Back/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+
+const app = express()
+
+app.use(helmet())
+app.use(cors())
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.send('<h1>Mi API</h1>')
+})
+
+app.use('/api', require('./routes'))
+
+app.listen(8080/*process.env.PORT*/, () => {
+  console.log(`Corriendo en ${process.env.PORT}`);
+})
